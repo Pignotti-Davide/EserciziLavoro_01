1e479a4bcd4e6a89bf92c4fcf24bfe99837de3b2
Davide Pignotti <davide110494@gmail.com>
2016-08-10 16:34:12 +0200

    commit service & directive built

--- Compito_AngularJs/assets/js/controllers/formCtrl.js
+++ Compito_AngularJs/assets/js/controllers/formCtrl.js
@@ -1,56 +1,27 @@
 var app = angular.module("myApp");
-var formCtrl=app.controller("formCtrl", function($scope) {
-	$scope.people=[];
-	$scope.createPerson= function(params){
-		var person=new Object();
-		person.name=params.name;
-		person.surname=params.surname;
-		person.age=10;
-		if(params.degree){
-			person.laurea="Si";
-		}
-		else person.degree="No";
-		$scope.people.push(person);
-	};
-	$scope.removePerson=function(j){
-		$scope.people.splice(j,1);
-	};
+var formCtrl=app.controller("formCtrl", function($scope, peopleService) {
+	
+	$scope.people=peopleService.getPeople();
+	$scope.createPerson=function(){
+		peopleService.savePerson($scope.person);
+	}
+	
+	$scope.removePerson=function(index){
+		peopleService.deletePerson(index)
+	}
+	
 	$scope.resetForm = function() {
-	    $scope.params.name=null;
-	    $scope.params.surname=null;
-	    $scope.params.date=null;
-	    $scope.params.degree=null;
-	    $scope.params.city="";
-	    $scope.params.street=null;
-	    $scope.params.cap=null;
-	    };
+		$scope.person.name=null;
+		$scope.person.surname=null;
+		$scope.person.date=null;
+		$scope.person.degree=null;
+		$scope.person.city=null;
+		$scope.person.street=null;
+		$scope.person.cap=null;
+	};
+	
 	$scope.showTable=function(){
-		if ($scope.people.length!=0) return true;
+		if (peopleService.getPeople().length!=0) return true;
 		else return false;
 	}
-});
-formCtrl.directive("onlyDigits", function () {
-
-	return {
-		restrict: "A",
-		require: "?ngModel",
-		link: function (scope, element, attrs, modelCtrl) {
-			modelCtrl.$parsers.push(function (inputValue) {
-				if (inputValue == undefined) return "";
-				var transformedInput = inputValue.replace(/[^0-9]/g, "");
-				if (transformedInput !== inputValue) {
-					modelCtrl.$setViewValue(transformedInput);
-					modelCtrl.$render();
-				}
-				return transformedInput;
-			});
-		}
-	};
-});
-
-$(function () {
-	$("#calendar").datetimepicker({
-		format: "DD/MM/YYYY"
-	});
-
 });
---
+++ Compito_AngularJs/assets/js/services/peopleService.js
@@ -0,0 +1,33 @@
+var app = angular.module("myApp");
+var peopleService=app.service('peopleService', function() {
+   
+	people=[];
+	person={
+			name:"",
+			surname:"",
+			birthday:"",
+			degree:"",
+			city:"",
+			street:"",
+			cap:""
+	};
+
+	this.savePerson= function(person){
+		var aPerson=new Object();
+		aPerson.name=person.name;
+		aPerson.surname=person.surname;
+		aPerson.age="";
+		if(person.degree){
+			aPerson.degree="Si";
+		}
+		else aPerson.degree="No";
+		people.push(aPerson);
+	};
+	this.deletePerson=function(index){
+		people.splice(index,1);
+	};
+	
+	this.getPeople=function(){
+		return people;
+	};
+});
