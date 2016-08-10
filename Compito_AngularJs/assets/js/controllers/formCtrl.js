220238278e5f7fbf7b8170a582a7333603d2ec9d
Davide Pignotti <davide110494@gmail.com>
2016-08-10 13:37:38 +0200

    commit

--- Compito_AngularJs/assets/js/controllers/formCtrl.js
+++ Compito_AngularJs/assets/js/controllers/formCtrl.js
@@ -1,16 +1,43 @@
 var app = angular.module("myApp");
-var appCtrl=app.controller("formCtrl", function($scope) {
-
+var formCtrl=app.controller("formCtrl", function($scope) {
+	$scope.people=[];
+	$scope.createPerson= function(params){
+		var person=new Object();
+		person.name=params.name;
+		person.surname=params.surname;
+		person.age=10;
+		if(params.degree){
+			person.laurea="Si";
+		}
+		else person.degree="No";
+		$scope.people.push(person);
+	};
+	$scope.removePerson=function(j){
+		$scope.people.splice(j,1);
+	};
+	$scope.resetForm = function() {
+	    $scope.params.name=null;
+	    $scope.params.surname=null;
+	    $scope.params.date=null;
+	    $scope.params.degree=null;
+	    $scope.params.city="";
+	    $scope.params.street=null;
+	    $scope.params.cap=null;
+	    };
+	$scope.showTable=function(){
+		if ($scope.people.length!=0) return true;
+		else return false;
+	}
 });
-appCtrl.directive('onlyDigits', function () {
+formCtrl.directive("onlyDigits", function () {
 
 	return {
-		restrict: 'A',
-		require: '?ngModel',
+		restrict: "A",
+		require: "?ngModel",
 		link: function (scope, element, attrs, modelCtrl) {
 			modelCtrl.$parsers.push(function (inputValue) {
-				if (inputValue == undefined) return '';
-				var transformedInput = inputValue.replace(/[^0-9]/g, '');
+				if (inputValue == undefined) return "";
+				var transformedInput = inputValue.replace(/[^0-9]/g, "");
 				if (transformedInput !== inputValue) {
 					modelCtrl.$setViewValue(transformedInput);
 					modelCtrl.$render();
