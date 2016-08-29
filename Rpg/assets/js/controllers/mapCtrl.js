8eb84a7d21b7c5096e3a9051a9e27277db2494d5
david <david@DESKTOP-MHN5GJ2>
2016-08-29 12:42:21 +0200

    commit

--- Rpg/assets/js/controllers/mapCtrl.js
+++ Rpg/assets/js/controllers/mapCtrl.js
@@ -2,7 +2,7 @@ var app = angular.module("myApp");
 var mapCtrl=app.controller("mapCtrl", function($scope){
 	
 	var maxHp=100;
-	var maxMp=100;
+	var maxMp=40;
 	$scope.player={name:"Jake",hp:100,atk:50,def:60,mp:40};
 	
 	var percentage=function(val,max){
